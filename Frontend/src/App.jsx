cd949b37330f9729e40720aa11c4440c3be2ebff
AjayNaik01 <naikajay952@gmail.com>
2025-05-16 14:03:04 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
+    const response = await axios.post("https://code-reviewer-backend-alpha.vercel.app", {
       code,
     });
     setReview(response.data);
